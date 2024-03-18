d88e13eaff9d8c03055daa3f88da726d39ed8bcf
Érico Bis <ericobis.santos@gmail.com>
2024-03-18 14:44:02 -0300

    First Commit

---
+++ character/scripts/main_character.gd
@@ -0,0 +1,48 @@
+extends CharacterBody2D
+
+var _state_machine
+
+@export_category("Variables")
+@export var _move_speed: float = 54.0
+@export var _acceleration: float = 0.2
+@export var _friction: float = 0.2
+
+@export_category("Objects")
+@export var _animation_tree: AnimationTree = null
+
+func _ready() -> void:
+	_state_machine = _animation_tree["parameters/playback"]
+	self.scale = Vector2(0.6, 0.6)
+	
+
+func _physics_process(_delta: float) -> void:
+	_move()
+	_animate()
+	move_and_slide()
+	
+func _move() -> void:
+	var _direction: Vector2 = Vector2(
+		Input.get_axis("move_left", "move_right"),
+		Input.get_axis("move_up", "move_down")
+	)
+	
+	
+	if _direction != Vector2.ZERO:
+		_animation_tree["parameters/Idle/blend_position"] = _direction
+		_animation_tree["parameters/Running/blend_position"] = _direction
+		
+		velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _acceleration)
+		velocity.y = lerp(velocity.y, _direction.normalized().y * _move_speed, _acceleration)
+		return
+	
+	velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _friction)
+	velocity.y = lerp(velocity.y, _direction.normalized().y * _move_speed, _friction)
+	
+	#velocity = _direction.normalized() * _move_speed
+
+func _animate() -> void:
+	if(velocity.length()) > 2:
+		_state_machine.travel("Running")
+		return
+	
+	_state_machine.travel("Idle")
