fdce9e4d6e31935a92faa63c8a247a18106cb0d8
Ana Xavier <anacarolina.oxavier@gmail.com>
2024-03-20 20:27:01 -0300

    decorating menu and buttons

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,9 +1,5 @@
 extends Control
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$controls/start.grab_focus()
-
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://levels/scenes/level_1.tscn")
 
