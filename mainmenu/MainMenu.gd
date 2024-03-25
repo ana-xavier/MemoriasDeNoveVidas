fb655ee90b9662a8c4822670611c4317917f974d
Ana Xavier <anacarolina.oxavier@gmail.com>
2024-03-25 14:27:04 -0300

    adding transition mode

--- mainmenu/MainMenu.gd
+++ mainmenu/MainMenu.gd
@@ -2,6 +2,9 @@ extends Control
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://levels/scenes/level_1.tscn")
+	
+	Transition.transition_menu()
+
 
 #func _on_controls_pressed() -> void:
 	#get_tree().change_scene_to_file("res://character/scenes/controls.tscn")
