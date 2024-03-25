4d4e708007f6b35d3436395d765a2e5229009630
Ana Xavier <anacarolina.oxavier@gmail.com>
2024-03-25 20:41:21 -0300

    changing background

--- mainmenu/MainMenu.gd
+++ mainmenu/MainMenu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://levels/scenes/level_1.tscn")
+	get_tree().change_scene_to_file("res://levels/scenes/house.tscn")
 	
 	Transition.transition_menu()
 
