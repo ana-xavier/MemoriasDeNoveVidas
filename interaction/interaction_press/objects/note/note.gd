830da8c0dcf194abf8cbeedad71f7f26f3172c5f
Érico Bis <ericobis.santos@gmail.com>
2024-05-02 15:26:56 -0300

    feat: improved notes states saving

--- interaction/interaction_press/objects/note/note.gd
+++ interaction/interaction_press/objects/note/note.gd
@@ -3,17 +3,19 @@ extends Node2D
 @onready var interaction_area: InteractionArea = $InteractionArea
 @onready var note_popup = get_tree().get_first_node_in_group("NotePopup")
 
-@export var id: int = 0
-@export var note_name: String = "" 
-@export var note_text: String = ""
-
-func init(_id, _note_name, _note_text, _position):
-	id = _id
-	note_name = _note_name
-	note_text = _note_text
-	position = _position
+@export var id: String = ""
+var note_name: String = "" 
+var note_text: String = ""
 
 func _ready():
+	if (InteractiveObjectsData.is_object_already_interacted(id)):
+		queue_free()
+		
+	var note_data = NotesData.get_note_by_id(id) as Note
+	if (note_data != null):
+		note_name = note_data.name
+		note_text = note_data.text
+		
 	interaction_area.interact = Callable(self, "_on_interact")
 	note_popup.close_note_popup()
 
@@ -21,5 +23,5 @@ func _on_interact():
 	note_popup.set_note_text(note_text)
 	note_popup.show_note_popup()
 	await note_popup.closed_note
-	DataManager.save_note_state(id, true)
+	InteractiveObjectsData.set_object_already_interacted(id)
 	queue_free()
