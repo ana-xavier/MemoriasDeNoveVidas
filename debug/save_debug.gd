62fe63738bd7ab0bfc4968e6ba81d1f61e23f686
Érico Bis <ericobis.santos@gmail.com>
2024-05-16 14:30:02 -0300

    refactor: singletons names and folder structure

--- debug/save_debug.gd
+++ debug/save_debug.gd
@@ -1,4 +1,4 @@
 extends Control
 
 func _on_button_pressed():
-	DataManager.save_game_data()	
+	SaveManager.save_game_data()	
--- singletons/InteractiveObjectsData.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var data: InteractiveObjectsDict = null
-
-func is_object_already_interacted(object_id: String) -> bool:
-	return data.is_object_already_interacted(object_id)
-	
-func set_object_already_interacted(object_id: String) -> void:
-	data.set_object_already_interacted(object_id)
