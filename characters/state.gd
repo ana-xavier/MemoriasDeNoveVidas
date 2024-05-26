307890c39205ad273feee63fcee1c640dd667423
Érico Bis <ericobis.santos@gmail.com>
2024-05-25 23:55:37 -0300

    feat: added finite state machine to manage characters states

---
+++ characters/state.gd
@@ -0,0 +1,17 @@
+@icon("res://gui/icons/editor/state_icon.png")
+extends Node
+class_name State
+
+signal state_transition
+
+func enter():
+	pass
+	
+func exit():
+	pass
+	
+func update(_delta: float):
+	pass
+	
+func transition():
+	pass
