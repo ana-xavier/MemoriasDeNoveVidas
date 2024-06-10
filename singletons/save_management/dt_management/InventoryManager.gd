935b5c59957b9cdbabf0650fcb48d1f09ebac83e
Ana Xavier <anacarolina.oxavier@gmail.com>
2024-06-10 18:48:36 -0300

    adding quest

--- singletons/save_management/dt_management/InventoryManager.gd
+++ singletons/save_management/dt_management/InventoryManager.gd
@@ -4,7 +4,6 @@ var data: PlayerInventory = null
 
 signal item_added(item_name: String, item_sprite: Texture)
 
-
 func add_item(id: String) -> void:
 	data.add_item(id)
 	var item: Item = ItemsData.get_item_data_by_id(id)
@@ -13,5 +12,8 @@ func add_item(id: String) -> void:
 func remove_item(id: String) -> void:
 	data.remove_item(id)
 
-func has_item(id: String) -> bool:
-	return data.has_item(id)
+func has_item(id: String, quantity: int = 1) -> bool:
+	return data.has_item(id, quantity)
+	
+func get_quantity(id: String) -> int:
+	return data.get_quantity(id)
