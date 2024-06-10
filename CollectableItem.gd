a479d1ad0c5ceec84cbbb7b0322b88f190872f6c
Ana Xavier <anacarolina.oxavier@gmail.com>
2024-06-10 19:02:22 -0300

    adding plank interaction mechanic

---
+++ CollectableItem.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@export var id_item: String = ""
+@export var sprite_item: Texture = null
+@export var id: String = ""
+
+@onready var sprite_node: Sprite2D = $Sprite2D
+@onready var interaction_area: InteractionArea = $InteractionArea
+
+func _ready():
+	if (InteractiveObjectsData.is_object_already_interacted(id)):
+		queue_free()
+	interaction_area.interact = Callable(self, "_on_interact")
+	
+func _on_interact():
+	InteractiveObjectsData.set_object_already_interacted(id)
+	InventoryManager.add_item(id_item)
+	queue_free()
+	
+
